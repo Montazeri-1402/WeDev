461f94004ed45f34f6586b99df31dc90886e45b1
ANoroozi82 <amirnoroozipc10@gmail.com>
2023-11-30 11:06:57 +0330

    initial js generator

---
+++ Task-5/data/notebooks.js
@@ -0,0 +1,26 @@
+const noteBooksList = [
+    {
+        bookName: "وصایای امام",
+        pageLenght: "300",
+        time: "1397",
+        size: "500"
+    },
+    {
+        bookName: "فارسی",
+        pageLenght: "150",
+        time: "1400",
+        size: "200"
+    }, 
+    {
+        bookName: "ریاضی کاربردی",
+        pageLenght: "350",
+        time: "1390",
+        size: "450"
+    }, 
+    {
+        bookName: "عربی",
+        pageLenght: "250",
+        time: "1402",
+        size: "600"
+    }
+]
