b68a9fe2c02ab6f8d8b7cf0ebcff59f39b3e6778
ANoroozi82 <amirnoroozipc10@gmail.com>
2023-12-07 12:46:42 +0330

    1402/09/16

--- Task-5/data/notebooks.js
+++ Task-5/data/notebooks.js
@@ -1,46 +1,31 @@
-class NoteClass {
-    constructor() {
-        this.noteBooksList = [
-            {
-                id: "1",
-                bookName: "وصایای امام",
-                pageLength: "300",
-                time: "1397",
-                size: "500"
-            },
-            {
-                id: "2",
-                bookName: "فارسی",
-                pageLength: "150",
-                time: "1400",
-                size: "200"
-            },
-            {
-                id: "3",
-                bookName: "ریاضی کاربردی",
-                pageLength: "350",
-                time: "1390",
-                size: "450"
-            },
-            {
-                id: "4",
-                bookName: "عربی",
-                pageLength: "250",
-                time: "1402",
-                size: "600"
-            }
-        ];
-    }
 
-    static deleteNoteBookById(bookId) {
-        // Use the class name to access noteBooksList
-        NoteClass.noteBooksList = NoteClass.noteBooksList.filter(book => book.id !== bookId);
-        return NoteClass.noteBooksList;
+module.exports = [
+    {
+        id: "1",
+        bookName: "وصایای امام",
+        pageLength: "300",
+        time: "1397",
+        size: "500"
+    },
+    {
+        id: "2",
+        bookName: "فارسی",
+        pageLength: "150",
+        time: "1400",
+        size: "200"
+    },
+    {
+        id: "3",
+        bookName: "ریاضی کاربردی",
+        pageLength: "350",
+        time: "1390",
+        size: "450"
+    },
+    {
+        id: "4",
+        bookName: "عربی",
+        pageLength: "250",
+        time: "1402",
+        size: "600"
     }
-
-    deleteNoteBookByIndex(bookIndex) {
-        // Implement this method as needed
-    }
-}
-
-module.exports = NoteClass;
+];
--- Task-5/routes/notes.js
+++ Task-5/routes/notes.js
@@ -1,16 +1,60 @@
 const express = require('express');
-const router = express.Router();
-const NoteClass = require('../data/Notebooks');  // Corrected import statement
+const router = express.Router(); // Corrected import statement
+const notebooks = require('../data/notebooks');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send(NoteClass.noteBooksList);  // Access the property on the instance, assuming noteBooksList is a public property
+router.get('/', function (req, res, next) {
+    res.send(notebooks);  // Access the property on the instance, assuming noteBooksList is a public property
 });
 
-router.get('/deleteById', function(req, res, next) {
+router.get('/deleteById', function (req, res, next) {
     const id = req.query.id;
-    console.log(">>>>>>> " + id);
-    res.json(NoteClass.deleteNoteBookById(id));  // Corrected reference to the class
+
+    for (let i = 0; i < notebooks.length; i++) {
+        if (notebooks[i].id === id) {
+            notebooks.splice(i, 1);
+        }
+    }
+
+    res.json(notebooks);
 });
+
+
+router.get('/deleteByIndex', function(req, res, next) {
+    const index = req.query.index;
+
+    notebooks.splice(index, 1);
+
+    res.json(notebooks);
+});
+
+router.get('/editNote', function (req, res, next) {
+    const id = req.query.id;
+    const key = req.query.key;
+    const value = req.query.value;
+    
+    for (let i = 0; i < notebooks.length; i++) {
+        if(notebooks[i].id === id){
+            notebooks[i].bookName = value;
+        }
+    }
+
+    res.json(notebooks);
+});
+
+router.get('/reNote', function(req, res, next) {
+    const body = req.body;
+    const id = req.query.id;
+
+    for (let i = 0; i < notebooks.length; i++) {
+        if(notebooks[i].id === id){
+            notebooks[i] = body;
+        } 
+    }
+
+    res.json(notebooks);
+});
+
+
 
 module.exports = router;
