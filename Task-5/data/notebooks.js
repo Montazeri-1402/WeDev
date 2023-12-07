6568ed4c0fe8f3d66aa49ed007439e7374be2296
Amir Noroozi <99032812+ANoroozi82@users.noreply.github.com>
2023-12-07 06:43:18 +0330

    1402/09/16 1

--- Task-5/data/notebooks.js
+++ Task-5/data/notebooks.js
@@ -1,32 +1,46 @@
-const noteBooksList = [
-    {
-        id:"1",
-        bookName: "وصایای امام",
-        pageLenght: "300",
-        time: "1397",
-        size: "500"
-    },
-    {
-        id:"2",
-        bookName: "فارسی",
-        pageLenght: "150",
-        time: "1400",
-        size: "200"
-    }, 
-    {
-        id:"3",
-        bookName: "ریاضی کاربردی",
-        pageLenght: "350",
-        time: "1390",
-        size: "450"
-    }, 
-    {
-        id:"4",
-        bookName: "عربی",
-        pageLenght: "250",
-        time: "1402",
-        size: "600"
+class NoteClass {
+    constructor() {
+        this.noteBooksList = [
+            {
+                id: "1",
+                bookName: "وصایای امام",
+                pageLength: "300",
+                time: "1397",
+                size: "500"
+            },
+            {
+                id: "2",
+                bookName: "فارسی",
+                pageLength: "150",
+                time: "1400",
+                size: "200"
+            },
+            {
+                id: "3",
+                bookName: "ریاضی کاربردی",
+                pageLength: "350",
+                time: "1390",
+                size: "450"
+            },
+            {
+                id: "4",
+                bookName: "عربی",
+                pageLength: "250",
+                time: "1402",
+                size: "600"
+            }
+        ];
     }
-]
 
-module.exports = noteBooksList;
+    static deleteNoteBookById(bookId) {
+        // Use the class name to access noteBooksList
+        NoteClass.noteBooksList = NoteClass.noteBooksList.filter(book => book.id !== bookId);
+        return NoteClass.noteBooksList;
+    }
+
+    deleteNoteBookByIndex(bookIndex) {
+        // Implement this method as needed
+    }
+}
+
+module.exports = NoteClass;
--- Task-5/routes/notes.js
+++ Task-5/routes/notes.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var notes = require('../data/notebooks');
+const express = require('express');
+const router = express.Router();
+const NoteClass = require('../data/Notebooks');  // Corrected import statement
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(notes);
+    res.send(NoteClass.noteBooksList);  // Access the property on the instance, assuming noteBooksList is a public property
 });
 
 router.get('/deleteById', function(req, res, next) {
-    notes.array.forEach(element => {
-        if(element.id == )
-    });
+    const id = req.query.id;
+    console.log(">>>>>>> " + id);
+    res.json(NoteClass.deleteNoteBookById(id));  // Corrected reference to the class
 });
 
 module.exports = router;
