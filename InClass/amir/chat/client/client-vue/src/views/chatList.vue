3986a5242e625e9c6183557e58a1154beb0e648f
Amir Noroozi <99032812+ANoroozi82@users.noreply.github.com>
2024-01-05 13:16:24 +0330

    1402/10/15 work on vue.js 1

---
+++ InClass/amir/chat/client/client-vue/src/views/chatList.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+import { ref } from 'vue';
+
+let chats = ref([]);
+axios.get('/api/users')
+  .then((res) => {
+    console.log({ res });
+    chats.value = res.data;
+  });
+</script>
+
+<template>
+    <div class="back">
+        <p v-for="i in chats">
+        {{ i.id }}
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.back {
+    display: flex;
+    flex-direction: column;
+}
+</style>
